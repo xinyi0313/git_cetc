8c866ec24934fe042c7e25dbce1c883e684c52cd
cetc28 <1138384982@qq.com>
2022-05-20 14:34:10 +0800

    dev branch first modify

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("hello git init");
 		System.out.println("modify by cetc28");
+		System.out.println("modify by dev cetc28");
 	}
 }
